75020d16c151a5b9659e2f6c12c9031abb15292c
Gunnstein Thomas Frøseth <gunnsteinthomas@gmail.com>
2018-11-11 18:31:11 +0100

    Brushed up example and readme, setup.py reworked

--- examples/example-rosenbrock.py
+++ examples/example-rosenbrock.py
@@ -10,12 +10,12 @@ class RosenbrockProblem(lahc.LateAcceptanceHillClimber):
 
         f(x, y) = (a-x)**2 + b(y-x**2)**2
 
-    Exact solution is found at (a, a**2) for b > 0
+    Global minima is located at (x, y) = (a, a**2) for b > 0
     """
-    def __init__(self, initial_state, a=1, b=100):
+    def __init__(self, initial_state, a=25, b=100):
+        super(RosenbrockProblem, self).__init__(initial_state=initial_state)
         self.rosenbrock_a = a
         self.rosenbrock_b = b
-        super(RosenbrockProblem, self).__init__(initial_state=initial_state)
 
     def move(self):
         A = random.normalvariate(0, 1)
@@ -37,18 +37,35 @@ if __name__ == '__main__':
     # guessing a solution
     initial_state = [-5.0, 5.0]
 
-    # initializing the problem
-    problem = RosenbrockProblem(initial_state)
-    problem.rosenbrock_a = 25
-
     # adjust the history length and update interval
-    problem.history_length = 5000
-    problem.updates_every = 1000
+    lahc.LateAcceptanceHillClimber.history_length = 5000
+    lahc.LateAcceptanceHillClimber.updates_every = 1000
+
+    # initializing the problem
+    prob = RosenbrockProblem(initial_state)
 
     # and run the Late Acceptance Hill Climber for a solution
-    problem.run()
-
-    print()
+    prob.run()
 
     # compare found solution to exact solution
-    print(problem.state, problem.exact_solution)
+    print()
+    print(prob.state, prob.exact_solution)
+    print(
+        "Solution with `deepcopy` strategy in: {0:.1f}s".format(
+            prob.time_end - prob.time_start))
+    print()
+
+
+    # The default behavoir of the algorithm is to use STL copy.deepcopy
+    # method, which is known to be slow at copying lists compared to
+    # slicing the list. Let us try the same problem with the slicing
+    # strategy.
+    prob_slice = RosenbrockProblem(initial_state)
+    prob_slice.copy_strategy = 'slice'
+
+    prob_slice.run()
+    print()
+    print(prob.state, prob.exact_solution)
+    print(
+        "Solution with `slice` strategy in: {0:.1f}s".format(
+            prob_slice.time_end - prob_slice.time_start))
